7c36cc6d8ba33d6027d3a4c40eedf0db053c63e1
huggman34 <70258867+huggman34@users.noreply.github.com>
2020-08-27 13:06:23 +0200

    Add files via upload

---
+++ loggin.php
@@ -0,0 +1,10 @@
+
+
+<?php
+ 
+ ?>
+ <form action="" method="post">
+     <input type="text" name="user" id="">
+     <input type="text" name="password" id="">
+     <input type="submit" value="logga in">
+ </form>
