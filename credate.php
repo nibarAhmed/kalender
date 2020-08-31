635c3f321226be6a3b8f6edb377ab9ba99aa4278
huggman34 <70258867+huggman34@users.noreply.github.com>
2020-08-31 14:10:38 +0200

    Add files via upload

---
+++ credate.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="credate.php" method="post">
+        <input type="date" name="datum">
+        <input type="text" name="event">
+        <input type="submit" value="klar">
+    </form>
+    <?php
+    //$_SESSION['userid'] = $userid;
+    $datum = $_POST['datum'];
+    $event = $_POST['event'];
+    $conn = new mysqli('localhost', 'root','','kalender');
+    $conn->set_charset("utf8");
+    if ($conn->connect_error) {
+     die("Connection failed: " . $conn->connect_error);
+     
+ }
+ $user='bbb';
+     $userid = 111;
+     $sql = "INSERT INTO events(event,datum,userid) VALUES ('$event','$datum','$userid')";
+     $conn->query($sql);
+    
+    ?>
+</body>
+</html>
---
+++ loggin.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php 
+session_start(
+$user=$_POST['user'];
+$password = $_POST['password'];
+
+$x = $_POST['x'];//holler koll på om du skapat skapat ett tidigare konto.
+   $conn = new mysqli('localhost', 'root','','kalender');
+   $conn->set_charset("utf8");
+   if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+$sql = "SELECT userid FROM login WHERE user='$user' AND password='$password'";
+
+$query = $conn->query($sql);
+
+$resultat = $query->fetch_assoc();
+switch($x){
+    case 1:        
+if (!empty($resultat)) {
+    echo "<form action='kal.html' method='post'>
+    <input type='submit' value='välkommen'>
+</form>";
+$resultt = $conn->query($sql);
+     if ($resultt->num_rows > 0) {
+        while($row = $resultt->fetch_assoc()) {
+           $userid=$row['userid'];
+        } }
+}else {
+    echo "<form action='loggin.html' method='post'>
+    <input type='submit' value='prövaigen'>
+</form>";
+
+    } 
+break;
+case 2:
+    if(!empty($resultat)) {
+        echo "<form action='logg.html' method='post'>
+        <input type='submit' value='det här användaren fis redan'>
+    </form>";
+    }else {
+        $userid = rand(1,100000000);
+        $sql = "INSERT INTO login (user,password,userid) VALUES ('$user','$password','$userid')";
+$conn->query($sql);
+        echo "<form action='lo.html' method='post'>
+        <input type='submit' value='välkommen'>
+    </form>";
+        } 
+break;
+}
+
+$_SESSION['userid']=$userid;
+$_SESSION['user']=$user;
+
+?>
+</body>
+</html>
